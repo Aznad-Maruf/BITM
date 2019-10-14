76ca44454790af6c261baaa0915e5cfd45f708cb
Maruf Robin <maruf.robin131313@gmail.com>
2019-10-14 18:16:36 +0600

    Refrigerator Added

---
+++ Refrigerator/RefrigeratorAppPractice3/Refrigerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrigeratorAppPractice3
+{
+    public class Refrigerator
+    {
+        public double MaximumWeight { private set; get; }
+        public double CurrentWeight { private set; get; }
+
+        public Refrigerator(double maximumWeight)
+        {
+            this.MaximumWeight = maximumWeight;
+            this.CurrentWeight = 0;
+        }
+
+        public bool EnterNewItem(int noOfItems, double weightPerUnit)
+        {
+            if (this.CurrentWeight+noOfItems*weightPerUnit>this.MaximumWeight)
+            {
+                return false;
+            }
+
+            CurrentWeight += (noOfItems * weightPerUnit);
+            return true;
+        }
+
+    }
+}
---
+++ Refrigerator/RefrigeratorAppPractice3/RefrigeratorUi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefrigeratorAppPractice3
+{
+    public partial class RefrigeratorUi : Form
+    {
+        private Refrigerator _refrigerator;
+
+        public RefrigeratorUi()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            _refrigerator = new Refrigerator(Convert.ToDouble(maxWeightTakeTextBox.Text));
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            if (_refrigerator.EnterNewItem(Convert.ToInt32(itemTextBox.Text), Convert.ToDouble(weightTextBox.Text)))
+            {
+                currentWeightTextBox.Text = _refrigerator.CurrentWeight.ToString();
+                remainingWeightTextBox.Text = (_refrigerator.MaximumWeight - _refrigerator.CurrentWeight).ToString();
+            }
+            else
+            {
+                //MessageBox.Show("Refrigerator is full man!");
+            }
+        }
+    }
+}
