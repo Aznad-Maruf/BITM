9ffe4e5a540a03ea485d75e1723f514f982a1679
Maruf Robin <maruf.robin131313@gmail.com>
2019-10-14 16:35:00 +0600

    Vehicle Added

---
+++ Vehicle/VechicleAppPractice2/Vehicle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VechicleAppPractice2
+{
+    public class Vehicle
+    {
+        private string VehicleName { get; }
+        private string RegNo { get; }
+        public double MinSpeed { get; private set; }
+        public double MaxSpeed { get; private set; }
+        public double AvgSpeed { get; private set; }
+        private double _sumOfSpeed;
+        private double _totalEntry;
+        private double NewSpeed;
+
+        public Vehicle()
+        {
+            this.MinSpeed = double.MaxValue;
+            this.MaxSpeed = double.MinValue;
+            this.AvgSpeed = 0;
+            this._sumOfSpeed = 0;
+            this._totalEntry = 0;
+        }
+
+        public Vehicle(string vehicleName, string regNo):this()
+        {
+            this.VehicleName = vehicleName;
+            this.RegNo = regNo;
+        }
+
+        /*
+        public double NewSpeed
+        {
+            set
+            {
+                this.NewSpeed = value;
+                _sumOfSpeed += NewSpeed;
+                _totalEntry++;
+                MinSpeed = Math.Min(MinSpeed, NewSpeed);
+                MaxSpeed = Math.Max(MaxSpeed, NewSpeed);
+                _sumOfSpeed += NewSpeed;
+                _totalEntry++;
+                AvgSpeed = _sumOfSpeed / _totalEntry;
+
+            }
+            get { return NewSpeed; }
+        }
+        */
+
+        public void setNewSpeed(double newSpeed)
+        {
+            this.NewSpeed = newSpeed;
+            _sumOfSpeed += NewSpeed;
+            _totalEntry++;
+            MinSpeed = Math.Min(MinSpeed, NewSpeed);
+            MaxSpeed = Math.Max(MaxSpeed, NewSpeed);
+            _sumOfSpeed += NewSpeed;
+            _totalEntry++;
+            AvgSpeed = _sumOfSpeed / _totalEntry;
+        }
+        
+
+    }
+}
---
+++ Vehicle/VechicleAppPractice2/VehicleUi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VechicleAppPractice2
+{
+    public partial class VehicleUi : Form
+    {
+        private Vehicle _aVehicle;
+
+        public VehicleUi()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            _aVehicle = new Vehicle(vehicleNameTextBox.Text,regNoTextBox.Text);
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            _aVehicle.setNewSpeed( Convert.ToDouble(speedTextBox.Text));
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            maxSpeedTextBox.Text = _aVehicle.MaxSpeed.ToString();
+            minSpeedTextBox.Text = _aVehicle.MinSpeed.ToString();
+            averageSpeedTextBox.Text = _aVehicle.AvgSpeed.ToString();
+        }
+    }
+}
